401bf37a752cb6bb8bd598809eb306605794a894
Mario <mario.vitoon@gmail.com>
2021-07-07 23:17:16 +0200

    Display notes on threads

--- api/log.ts
+++ api/log.ts
@@ -172,6 +172,7 @@ export default async (req: VercelRequest, res: VercelResponse) => {
     <div class="main">
     		<div>
             <h2>Log from user ${data.recipient.username}#${data.recipient.discriminator}</h2>
+            ${data.note ? '<p>Note: ${data.note}</p>' : ''}
             <hr>
         </div>
         
@@ -183,4 +184,4 @@ export default async (req: VercelRequest, res: VercelResponse) => {
 </html>
 `);
 
-}
+};
